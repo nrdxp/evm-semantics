f1c219a9db7c936012cc441bc671220f7cabb9cc
Timothy DeHerrera <tim.deherrera@iohk.io>
2021-11-01 16:13:42 -0600

    wip

--- default.nix
+++ default.nix
@@ -1,47 +1,38 @@
-let
-  sources = import ./nix/sources.nix {};
-  pinned = import sources."nixpkgs" {};
-in
+{ pkgs ? import inputs."nixpkgs" { }, inputs ? import ./nix/sources.nix { }
+, release ? null, kframework ? let
+  tag = pkgs.lib.fileContents ./deps/k_release;
+  url = "https://github.com/kframework/k/releases/download/${tag}/release.nix";
+  args = import (builtins.fetchurl { inherit url; });
+  src = pkgs.fetchgit args;
+in import src {
+  release = if release == null then pkgs.stdenv.isLinux else false;
+}
 
-{ pkgs ? pinned
-, release ? null
 }:
-
-let release_ = release; in
-
 let
+
   inherit (pkgs) lib;
-  ttuegel = import sources."nix-lib" { inherit pkgs; };
+  ttuegel = import inputs."nix-lib" { inherit pkgs; };
 
-  release = if release_ == null then pkgs.stdenv.isLinux else false;
-
-  kframework =
-    let
-      tag = lib.fileContents ./deps/k_release;
-      url = "https://github.com/kframework/k/releases/download/${tag}/release.nix";
-      args = import (builtins.fetchurl { inherit url; });
-      src = pkgs.fetchgit args;
-    in import src { inherit release; };
   inherit (kframework) k haskell-backend clang;
   # The following llvm-backend directory is needed at build time by kevm, but it's missing
   # from the llvm-backend nix package, so we override the postInstall phase to copy it in
   # the nix store.
   # NOTE: Move this to the llvm-backend repository?
-  llvm-backend = kframework.llvm-backend.overrideAttrs (old:
-    { postInstall = if old ? postInstall then old.postInstall else "" + ''
+  llvm-backend = kframework.llvm-backend.overrideAttrs (old: {
+    postInstall = if old ? postInstall then
+      old.postInstall
+    else
+      "" + ''
         mkdir -p $out/lib/cmake/kframework
         cp -r ../cmake/* $out/lib/cmake/kframework/;
-        '';
-    }
-  );
+      '';
+  });
   llvmPackages = pkgs.llvmPackages_10;
-in
 
-let
-  inherit (pkgs) callPackage;
-in
+in let inherit (pkgs) callPackage;
 
-let
+in let
   src = ttuegel.cleanGitSubtree {
     name = "evm-semantics";
     src = ./.;
@@ -61,8 +52,5 @@ let
     inherit k haskell-backend llvm-backend clang;
     inherit (pkgs.python2Packages) python;
   };
-  default =
-    {
-      inherit kevm;
-    };
+  default = { inherit kevm; };
 in default
